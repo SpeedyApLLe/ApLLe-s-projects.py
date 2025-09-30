80105695e41b3d5cd768210572615b1249dc284f
SpeedyApLLe <156204766+SpeedyApLLe@users.noreply.github.com>
2025-09-30 18:39:49 +0300

    .py calculator
    it's a .py version of my simple calculator.(my first project)

---
+++ script.py
@@ -0,0 +1,12 @@
+number = int(input("write your first number:"))
+
+number2 = int(input("write your second number:"))
+
+print(number + number2, "plus +")
+print(number - number2, "minus -")
+print(number * number2, "Multiplication *")
+print(number / number2, "Division /")
+print(number % number2, "Modulus %")
+print(number // number2, "Floor division //")
+
+input("press enter to exit..." )
